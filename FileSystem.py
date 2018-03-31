1dc8b74e822e4d111a0eec982e1d364f70fada28
Hesham97 <hesham_magdy97@hotmail.com>
2018-03-31 19:22:03 +0200

    For Z

--- FileSystem.py
+++ FileSystem.py
@@ -3,7 +3,6 @@ import math
 
 
 class IFileSystemElement:
-
     __metaclass__ = ABCMeta
 
     @abstractmethod
@@ -33,22 +32,29 @@ class DirectoryComposite(IFileSystemElement):
                 return False
         return True
 
-    def show(self,indent):
-        print(indent*" "+"| "+self.__name)
+    def show(self, indent):
+        print(indent * " " + "| " + self.__name)
         for file in self.__files:
             file.show(indent + 2)
 
     def create_dir(self, root_name, name):
         if self.__name == root_name:
-            self.__files.add(DirectoryComposite(name))
+            dir_new = DirectoryComposite(name)
+            self.__files.add(dir_new)
+            return dir_new
         for file in self.__files:
-            file.create_dir(root_name,name)
+            file.create_dir(root_name, name)
+        return False
 
-    def create_file(self, root_name, name):
+    def create_file(self, root_name, name, size):
         if self.__name == root_name:
-            self.__files.add(File(name))
+            file_new = File(name, size)
+            self.__files.add(file_new)
+            return file_new
         for file in self.__files:
-            file.create_file(root_name,name)
+            file.create_file(root_name, name, size)
+
+        return False
 
     def remove_file(self, name):
         s = ""
@@ -62,18 +68,22 @@ class DirectoryComposite(IFileSystemElement):
 
 class File(IFileSystemElement):
 
-    def __init__(self, name):
+    def __init__(self, name, size):
         self.__name = name
-        self.__size = 0
+        self.__size = size
+        self.__block_numbers = []
 
-    def show(self,intent):
-        print(intent * " " + "-- " +self.__name)
+    def show(self, intent):
+        print(intent * " " + "-- " + self.__name)
 
     def create_file(self, root_name, name): pass
 
     def create_dir(self, root_name, name): pass
 
     def remove_file(self, name): pass
+
+    def add_block_number(self, block):
+        self.__block_numbers.append(block)
 
     def get_name(self):
         return self.__name
@@ -92,11 +102,26 @@ class FileSystem:
 
     def __init__(self, disk_size, block_size):
         extent_size = math.ceil(disk_size / block_size)
-        self.__extent = [x for x in range(0,extent_size)]
+        self.__extent = [x for x in range(0, extent_size)]
         print([x for x in self.__extent])
         self.__root = DirectoryComposite("root")
         self.__disk_size = disk_size
         self.__block_size = block_size
+
+    def allocate_blocks(self, file):
+        # TODO FIND WAY TO LINK NUMBERS
+        counter, last = 0, 0
+        blocks_required = int(file.get_size() / self.__block_size)
+        print(file.get_size())
+        for i in self.__extent:
+            if i == self.__extent[i] and counter < blocks_required:
+                file.add_block_number(i)
+                print(self.__extent[i])
+                self.__extent[self.__extent.index(last)] = i
+                #last = i
+                counter += 1
+
+        return True
 
     def print_state(self):
         self.__root.show(1)
@@ -109,12 +134,14 @@ class FileSystem:
             for title in names[1:-1]:
                 if self.__root.check_found(title):
                     return False
-            dir_name = names[len(names)-1]
-            parent = names[len(names)-2]
-            self.__root.create_dir(parent,dir_name)
+            dir_name = names[len(names) - 1]
+            parent = names[len(names) - 2]
+            '''Now here i get the dir created either 0'''
+            print(self.__root.create_dir(parent, dir_name))
             return True
 
-    def create_file(self, name,size):
+    def create_file(self, name, size):
+        ###TODO Needing to check if there is available number of blocks
         names = name.split("/")
         if names[0] != "root" or size > self.__disk_size:
             return False
@@ -122,9 +149,12 @@ class FileSystem:
             for title in names[1:-1]:
                 if self.__root.check_found(title):
                     return False
-            dir_name = names[len(names)-1]
-            parent = names[len(names)-2]
-            self.__root.create_file(parent,dir_name)
+            dir_name = names[len(names) - 1]
+            parent = names[len(names) - 2]
+            file_new = self.__root.create_file(parent, dir_name, size)
+            #self.allocate_blocks(file_new)
+            print([x for x in range(0, len(self.__extent))])
+            #TODO eb2i n2si el size
             return True
 
 
@@ -140,5 +170,4 @@ while True:
         print(vfs.create_dir(command[1]))
 
     vfs.print_state()
-
 
